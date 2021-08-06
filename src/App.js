d634a96aecef7c18fff102349f2305c9a7992453
April Breedlove <aprilbreedlove@Aprils-MacBook-Pro.local>
2021-08-06 17:06:20 -0400

    Created delete button functionality

--- src/App.js
+++ src/App.js
@@ -24,12 +24,19 @@ function App() {
     },
 ])
 
+// Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks to Show'}
     </div>
   );
 }
+
+// React JS Crash Course 2021 57:00
 
 export default App;
