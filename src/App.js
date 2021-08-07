f3318d581effb7d761d574e411e51f8a7696bfda
April Breedlove <aprilbreedlove@Aprils-MBP.attlocal.net>
2021-08-07 14:25:56 -0400

    Created reminder toggle functionality

--- src/App.js
+++ src/App.js
@@ -29,14 +29,19 @@ const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+}
+
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks to Show'}
+      {tasks.length > 0 ? 
+      (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks to Show')}
     </div>
   );
 }
 
-// React JS Crash Course 2021 57:00
 
 export default App;
